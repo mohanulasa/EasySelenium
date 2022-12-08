d696fa1561d9df83a463c5dadd99a06be1a5fcc8
ulasamohan <ulasamohan1992@gmail.com>
2022-12-08 21:03:57 +0530

    additing the project

---
+++ src/test/java/com/testBox/RadioBtnPage.java
@@ -0,0 +1,16 @@
+package com.testBox;
+
+import org.testng.annotations.Test;
+
+public class RadioBtnPage {
+
+	@Test
+	public void RadioBtnPage_TC001() {
+		System.out.println("Radio Btn Page & Testcase 001");
+	}
+	
+	@Test
+	public void RadioBtnPage_TC002() {
+		System.out.println("Radio Btn Page & Testcase 002");
+	}
+}
---
+++ src/test/java/com/testBox/TextBoxesPage.java
@@ -0,0 +1,21 @@
+package com.testBox;
+
+import org.testng.annotations.Test;
+
+public class TextBoxesPage {
+
+	@Test
+	public void TextBoxPage_TC001() {		
+		System.out.println("Inside Textbox Page & Testcase 001..!");
+	}
+	
+	@Test
+	public void TextBoxPage_TC002() {		
+		System.out.println("Inside Textbox Page & Testcase 002..!");
+	}
+
+	@Test
+	public void TextBoxPage_TC003() {		
+		System.out.println("Inside Textbox Page & Testcase 003..!");
+	}
+}
